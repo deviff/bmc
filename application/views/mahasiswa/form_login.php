ccd8834564b25c3a8e423c09469d92830c1a7bfb
Devi Firdaus <dfedogawa3@gmail.com>
2013-05-20 19:31:41 +0700

    Push Aplikasi Saat Kursus Codeigniter

---
+++ application/views/mahasiswa/form_login.php
@@ -0,0 +1,20 @@
+<?php echo form_open('mahasiswa/login/', 'mahasiswa_login'); ?>
+<table>
+	<tr>
+		<td>Email : </td>
+		<td><?php echo 
+		form_input('email','','placeholder=youremail@email.com'); ?>
+		</td>
+	</tr>
+	<tr>
+		<td>Password :</td>
+		<td><?php echo form_password('password'); ?></td>
+	</tr>
+	<tr>
+		<td colspan='2'><?php 
+		echo form_submit('submit', 'Login'); ?>
+		<?php echo form_reset('reset','Reset'); ?>
+	</td>
+	</tr>
+</table>
+<?php echo form_close(); ?>
